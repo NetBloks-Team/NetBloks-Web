e588a768b9a7d35af393e2035a0651a9503955df
MaxedPC08 <maxedpc@outlook.com>
2025-04-05 12:09:14 -0700

    MVP UI

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Python version: 3.13
 import os
 from PyQt6.QtWidgets import QApplication, QMainWindow, QComboBox, QVBoxLayout, QWidget, QPushButton, QInputDialog
 from PyQt6.QtWidgets import QHBoxLayout
-import qtWidgets
+import application
 from gemini_gen import gemini_gen
 
 #-----Function Section-----
@@ -22,28 +22,12 @@ def main():
     with open("llm_output.py", "w", encoding="utf-8") as f:
         f.write(gemini_gen())
     os.system("python llm_output.py")
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Workspace Selector")
         
-        # Create a combo box as a placeholder for qtWidgets.WorkspaceSelecter
-        self.test_widget = qtWidgets.CodeEditor() #CodeBlock("Conv2d", ["kernel", "Stride", "Padding"])
-        self.test_widget.setGeometry(0, 0, 200, 100)
-        # self.workspace_selector.addItems(["Workspace 1", "Workspace 2", "Workspace 3"])
-        
-        # Set up the central widget and layout
-        central_widget = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(self.test_widget)
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
         
 
 def qt_main():
     app = QApplication([])
-    window = MainWindow()
+    window = application.MainWindow()
     window.show()
     app.exec()    
 
