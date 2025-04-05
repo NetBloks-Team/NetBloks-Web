4f4fba708c24814968a2c38f500872e17359fa21
Kevin <pk585261@outlook.com>
2025-04-05 08:10:23 -0700

    Google Gemini API Implemented

--- main.py
+++ main.py
@@ -5,11 +5,24 @@ Authors: Max Clemetsen and Kevin Pettibone
 Python version: 3.13.2
 """
 
+#-----Import Section-----
+
+from google import genai
+import os
+
+#-----Function Section-----
+
 def main():
     """
     docsting thing goes here
     """
-    pass
+    print("You are using API key:", os.environ["GEMINI_KEY"])
+    client = genai.Client(api_key=os.environ["GEMINI_KEY"])
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-thinking-exp-01-21", contents="Explain AI to me in simple terms"
+    )
+    print(response.text)
+
 
 if __name__ == "__main__":
     main()
