6255c91b759aef55aca281792c393b17b1a71e54
Kevin <pk585261@outlook.com>
2025-04-05 09:32:48 -0700

    Syncing codebase

--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 An AI project for the UNR ACM 2025 Hackathon
 
 Authors: Max Clemetsen and Kevin Pettibone
-Python version: 3.13.2
+Python version: 3.13
 """
 
 #-----Import Section-----
 
 import os
 from google import genai
+from gemini_gen import gemini_gen
 
 #-----Function Section-----
 
@@ -16,19 +17,8 @@ def main():
     """
     docstring thing goes here
     """
-    print("You are using API key:", os.environ["GEMINI_KEY"])
-    #Make sure to put your Gemini API key in the environment variables
-    client = genai.Client(api_key=os.environ["GEMINI_KEY"])
-    with open("nn.json", "r", encoding="utf-8") as f:
-        prompt = f.read()
-    full_prompt = "Please generate python code for a pytorch neural network that has the following parameters:\n" + prompt +"\nOnly give code, and do not reply with anything else. Give the code as an entire file that can be executed."
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-thinking-exp-01-21", contents=full_prompt
-    )
-    output = response.text.strip("```").removeprefix("python\n")
-    print(output)
     with open("llm_output.py", "w", encoding="utf-8") as f:
-        f.write(output)
+        f.write(gemini_gen())
     os.system("python llm_output.py")
 
 if __name__ == "__main__":
