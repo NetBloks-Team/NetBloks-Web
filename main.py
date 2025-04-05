acd6ac2ff5873a2c571097b26a61a3cb6f7b9fc0
Kevin <pk585261@outlook.com>
2025-04-04 21:56:25 -0700

    Added some docstrings

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+"""
+An AI project for the UNR ACM 2025 Hackathon
+
+Authors: Max Clemetsen and Kevin Pettibone
+Python version: 3.13.2
+"""
+
 def main():
+    """
+    docsting thing goes here
+    """
     pass
 
 if __name__ == "__main__":
