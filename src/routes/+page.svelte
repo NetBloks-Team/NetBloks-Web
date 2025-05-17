782ba1409504a202b7656d66f91b75f331431aec
Kevin <githubpublic@desl8.33mail.com>
2025-05-16 19:44:35 -0700

    Changing the directories around

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script context="module" lang="ts">
+    // Placeholder for module script
+    let count = 0;
+</script>
+
+<p>You have clicked {count} times</p>
+<button on:click={() => count++}>Click me!</button>
